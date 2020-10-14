6b6f4026c54f58bfb8a26fbdb8792897e0fe62a8
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-10-14 23:23:50 +0200

    Fetch data & Manipulate DOM

---
+++ script.js
@@ -0,0 +1,52 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from the apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    // Check the quote length in order to determine styling
+    if (quote.text.length > 120)
+        quoteText.classList.add('long-quote');
+    else
+        quoteText.classList.remove('long-quote');
+
+    quoteText.textContent = quote.text;
+
+    // Check if the author field is blank; If blank, replace it with 'Unknown'
+    if (!quote.author)
+        authorText.textContent = 'Unknown';
+    else
+        authorText.textContent = quote.author;
+}
+
+// Get Quote from API
+async function getQuote() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+twitterBtn.addEventListener('click', tweetQuote);
+newQuoteBtn.addEventListener('click', newQuote);
+
+// On Load
+getQuote();
